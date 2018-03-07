fdc7610d740aa775d6805047a10ec57192091d08
Franklin Kong <franklintigerkong@gmail.com>
2018-03-06 16:35:34 -0800

    Updated

--- data.R
+++ data.R
@@ -1,9 +1,34 @@
 library("maps")
 library("dplyr")
-library("ggplot2")
-
+library("plotly")
 
 data <- read.csv("data.csv", stringsAsFactors = FALSE)
 NY <- map_data("county") %>% filter(region == "new york")
-print(ggplot(NY,aes(long, lat, group=group))+geom_polygon())
+data$County <- tolower(data$County)
+data_2014 <- data %>% filter(Year == 2014)
+colnames(NY)[which(colnames(NY) == "subregion")] <- "County"
 
+full_data <- left_join(NY, data_2014)
+
+g <- list(
+  scope = 'usa',
+  projection = list(type = 'albers usa'),
+  lakecolor = toRGB('white')
+)
+
+
+p <- full_data %>%
+  group_by(group) %>%
+  plot_ly(
+    x = ~long,
+    y = ~lat,
+    fillcolor = 'white',
+    hoverinfo = "none"
+  ) %>% 
+  add_polygons(
+    line = list(color = 'black', width = 0.5)) %>%
+  layout(
+    geo = g
+  )  
+
+print(p)
