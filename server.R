3ad886ab9cf60e1fb22a049a0034eee91f4c3fae
Franklin Kong <franklintigerkong@gmail.com>
2018-03-07 00:48:13 -0800

    server update

--- server.R
+++ server.R
@@ -3,16 +3,16 @@ shinyServer(function(input, output) {
   graphMap <- function(var, year) {
     data <- df %>% filter(Year == year)
     full_data <- left_join(NY, data_2014)
-    gg <- ggplot(full_data, aes(long,lat, group=group, fill=eval(as.name(var)), text=County)) + 
-      geom_polygon( col="white")
-    ggplotly(gg)
+    
+    gg <- ggplot(full_data, aes(long,lat, group=group, fill=eval(as.name(var)))) + 
+      geom_polygon( col="white") + labs(color = "Cylinders"))
+
+    ggplotly(gg, tooltip="sas")
   }
-  
   currentGraph <- reactive({graphMap(input$crimeChoices, input$animation)})
   
   output$graph <- renderPlotly(
     currentGraph()
   )
-  
 })
 
