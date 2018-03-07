af911e048954f482fc94434d3990580aa474d5b9
Alanna Hu <alanna@Alannas-Air.local>
2018-03-07 03:23:58 -0800

    Format fix

--- data.R
+++ data.R
@@ -3,9 +3,8 @@ library("dplyr")
 library("plotly")
 
 df <- read.csv("data.csv", stringsAsFactors = FALSE)
-NY <- map_data("county") %>% filter(region == "new york") %>% select(-order, -region)
+NY <-
+  map_data("county") %>% filter(region == "new york") %>% select(-order,-region)
 df$County <- tolower(df$County)
 data_2014 <- df %>% filter(Year == 2014)
 colnames(NY)[which(colnames(NY) == "subregion")] <- "County"
-
-
