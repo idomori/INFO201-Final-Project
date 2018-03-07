f390a44b72f4ff2217307a5f31d9150192b3df31
Alanna Hu <alanna@Alannas-Air.local>
2018-03-07 03:21:45 -0800

    about us updated

--- ui.R
+++ ui.R
@@ -38,37 +38,23 @@ shinyUI(fluidPage(
                  )
                )
              )),
-    tabPanel("Explore Data",
-             # source("data.R")
-             basicPage(
-               h2("Index, Violent, Property, and Firearm Rates By County: Beginning 1990"),
-               DT::dataTableOutput("mytable")
-             ),
-             verbatimTextOutput("summary")
+    tabPanel(
+      "Explore Data",
+      # source("data.R")
+      basicPage(
+        h2(
+          "Index, Violent, Property, and Firearm Rates By County: Beginning 1990"
+        ),
+        DT::dataTableOutput("mytable")
+      ),
+      verbatimTextOutput("summary")
     ),
     navbarMenu(
       "More",
       tabPanel("About the Project",
                fluidRow(includeMarkdown("tab3.Rmd"))),
-      tabPanel("About Us"#,
-               # fluidRow(
-               #    column(9,
-               #       #includeMarkdown("aboutUs.md")
-               #     ),
-               #     column(3,
-               #       # img(class="img-polaroid",
-               #       #   src=paste0("http://upload.wikimedia.org/",
-               #       #   "wikipedia/commons/9/92/",
-               #       #   "1919_Ford_Model_T_Highboy_Coupe.jpg")),
-               #       # tags$small(
-               #   "Source: Photographed at the Bay State Antique ",
-               #   "Automobile Club's July 10, 2005 show at the ",
-               #   "Endicott Estate in Dedham, MA by ",
-               #   a(href="http://commons.wikimedia.org/wiki/User:Sfoskett",
-               #     "User:Sfoskett")
-               #  )
-               # )
-               # )                        )
-      )
+      tabPanel("About Us",
+               fluidRow(includeMarkdown("infoaboutme.rmd")))
     )
-  )))
+  )
+))
