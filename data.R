bc26ac805029868e1126688f12a095f2177f43b8
Franklin Kong <franklintigerkong@gmail.com>
2018-03-06 21:52:30 -0800

    server and ui updated

--- data.R
+++ data.R
@@ -2,25 +2,10 @@ library("maps")
 library("dplyr")
 library("plotly")
 
-data <- read.csv("data.csv", stringsAsFactors = FALSE)
-NY <- map_data("county") %>% filter(region == "new york")
-data$County <- tolower(data$County)
-data_2014 <- data %>% filter(Year == 2014)
+df <- read.csv("data.csv", stringsAsFactors = FALSE)
+NY <- map_data("county") %>% filter(region == "new york") %>% select(-order, -region)
+df$County <- tolower(df$County)
+data_2014 <- df %>% filter(Year == 2014)
 colnames(NY)[which(colnames(NY) == "subregion")] <- "County"
 
-full_data <- left_join(NY, data_2014) %>% group_by(group)
 
-
-
-
-
-
-
-
-
-p <- ggplot(full_data, aes(long,lat, group= group, fill=Population, text=County)) + geom_polygon( col="white")
-
-a <- ggplotly(p)
-
-
-print(a)
---
+++ server.R
@@ -0,0 +1,18 @@
+shinyServer(function(input, output) {
+  
+  graphMap <- function(var, year) {
+    data <- df %>% filter(Year == year)
+    full_data <- left_join(NY, data_2014)
+    gg <- ggplot(full_data, aes(long,lat, group=group, fill=eval(as.name(var)), text=County)) + 
+      geom_polygon( col="white")
+    ggplotly(gg)
+  }
+  
+  currentGraph <- reactive({graphMap(input$crimeChoices, input$animation)})
+  
+  output$graph <- renderPlotly(
+    currentGraph()
+  )
+  
+})
+
