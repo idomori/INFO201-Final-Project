b324031500ae0b5925703fa1963b9bf499b3f90e
Alanna Hu <alanna@Alannas-Air.local>
2018-03-07 02:25:33 -0800

    Updated about this project

--- ui.R
+++ ui.R
@@ -1,29 +1,40 @@
 source("data.R")
 library(markdown)
 library(shinythemes)
-shinyUI(fluidPage( theme= shinytheme("darkly"),
-  navbarPage("New York Crime Rate",
+shinyUI(fluidPage(
+  theme = shinytheme("darkly"),
+  navbarPage(
+    "New York Crime Rate",
     tabPanel("Visualize",
-      sidebarLayout(
-        sidebarPanel(
-          selectInput("crimeChoices", label = "Choose the type of crime to visualize", 
-                            choices = list("Violent Crimes" = "Violent.Rate", 
-                            "Property Crimes" = "Property.Rate",
-                            "Firearm Crimes" = "Firearm.Rate"), 
-                             selected = "Violent.Rate"),
-          fluidRow(column(3))
-        ),
-        mainPanel(
-          plotlyOutput("graph"),  
-              sliderInput("animation", "Year",
-                          min = 1990, max = 2015,
-                          value = 1, step = 1,
-                          animate = animationOptions(interval = 900, loop = TRUE),
-                          width = '100%',
-                          sep = "")
-            )
-        )
-    ),
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput(
+                   "crimeChoices",
+                   label = "Choose the type of crime to visualize",
+                   choices = list(
+                     "Violent Crimes" = "Violent.Rate",
+                     "Property Crimes" = "Property.Rate",
+                     "Firearm Crimes" = "Firearm.Rate"
+                   ),
+                   selected = "Violent.Rate"
+                 ),
+                 fluidRow(column(3))
+               ),
+               mainPanel(
+                 plotlyOutput("graph"),
+                 sliderInput(
+                   "animation",
+                   "Year",
+                   min = 1990,
+                   max = 2015,
+                   value = 1,
+                   step = 1,
+                   animate = animationOptions(interval = 900, loop = TRUE),
+                   width = '100%',
+                   sep = ""
+                 )
+               )
+             )),
     tabPanel("Explore Data",
          # library(shiny)
          # library(DT)
@@ -55,31 +66,29 @@ shinyUI(fluidPage( theme= shinytheme("darkly"),
          # }
         verbatimTextOutput("summary")
     ),
-    navbarMenu("More",
+    navbarMenu(
+      "More",
       tabPanel("About the Project",
-        fluidRow(
-            includeMarkdown("tab3.Rmd")
-        )), 
+               fluidRow(includeMarkdown("tab3.Rmd"))),
       tabPanel("About Us"#,
-        # fluidRow(
-        #    column(9,
-        #       #includeMarkdown("aboutUs.md")
-        #     ),
-        #     column(3,
-        #       # img(class="img-polaroid",
-        #       #   src=paste0("http://upload.wikimedia.org/",
-        #       #   "wikipedia/commons/9/92/",
-        #       #   "1919_Ford_Model_T_Highboy_Coupe.jpg")),
-        #       # tags$small(
-              #   "Source: Photographed at the Bay State Antique ",
-              #   "Automobile Club's July 10, 2005 show at the ",
-              #   "Endicott Estate in Dedham, MA by ",
-              #   a(href="http://commons.wikimedia.org/wiki/User:Sfoskett",
-              #     "User:Sfoskett")
-             #  )
-             # )                      
-         # )                        
-       )
+               # fluidRow(
+               #    column(9,
+               #       #includeMarkdown("aboutUs.md")
+               #     ),
+               #     column(3,
+               #       # img(class="img-polaroid",
+               #       #   src=paste0("http://upload.wikimedia.org/",
+               #       #   "wikipedia/commons/9/92/",
+               #       #   "1919_Ford_Model_T_Highboy_Coupe.jpg")),
+               #       # tags$small(
+               #   "Source: Photographed at the Bay State Antique ",
+               #   "Automobile Club's July 10, 2005 show at the ",
+               #   "Endicott Estate in Dedham, MA by ",
+               #   a(href="http://commons.wikimedia.org/wiki/User:Sfoskett",
+               #     "User:Sfoskett")
+               #  )
+               # )
+               # )                        )
+      )
     )
-  )
-))
+  )))
